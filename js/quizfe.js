006204b5ba9f437c40deb66fd4547c68f8b0aa81
Abhinav Kumar <abhinav@outlook.com>
2016-11-30 20:05:49 +0530

    Add routes

--- js/quizfe.js
+++ js/quizfe.js
@@ -1,5 +1,33 @@
-var quiz = angular.module('quizApp')
+var app = angular.module('quizApp', ['ngRoute'])
 
-quiz.controller('loginController',['$scope', function($scope){
+app.config(function($routeProvider) {
+  $routeProvider.when("/", {
+    templateUrl: "templates/dashboard.html",
+    controller: "homeController"
+  }).when("/addquiz", {
+    templateUrl: "templates/addquiz.html",
+    controller: "addQuizController"
+  }).when("/quiz", {
+    templateUrl: "templates/quiz.html",
+    controller: "QuizzerController"
+  }).when("/quiz1", {
+    templateUrl: "templates/quiz.html",
+    controller: "QuizzerController"
+  }).when("/quiz2", {
+    templateUrl: "templates/quiz.html",
+    controller: "QuizzerController"
+  }).when("/quiz3", {
+    templateUrl: "templates/quiz.html",
+    controller: "QuizzerController"
+  }).otherwise({
+    redirectTo: '/login'
+  })
+})
+
+app.controller('homeController',['$scope', function($scope){
+
+}])
+
+app.controller('mainController',['$scope', function($scope){
   
 }])
