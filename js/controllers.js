7eb611b9bea8a4c439ed270b2dc2e24be90d72e1
Abhinav Kumar <abhinav@outlook.com>
2016-12-08 21:28:47 +0530

    Changed style of quiz

--- js/controllers.js
+++ js/controllers.js
@@ -1,34 +1,47 @@
 app.controller('homeCtrl', ['$scope', function ($scope) {
 
-}])
+}]);
 
 app.controller('mainCtrl', ['$scope', function ($scope) {
 
-}])
+}]);
 
 app.controller('quizListCtrl', function ($scope, quizService) {
 
   quizService.getAllQuizzes().then(function (qs) {
-    $scope.quizzes = qs
+    $scope.quizzes = qs;
     console.log(qs)
   }, function (error) {
     $scope.status = 'Cannot load data'
   })
-})
+});
 
 app.controller('quizCtrl', function ($scope, $routeParams, quizService) {
-  var quizid = $routeParams.quizid
+  var quizid = $routeParams.quizid;
 
   quizService.getAllQuizzes().then(function (qarray) {
-    console.log(qarray)
+    console.log(qarray);
     for (var i = 0; i < qarray.length; i++)
       if (qarray[i].id == quizid) {
-        $scope.quiz = qarray[i]
+        $scope.quiz = qarray[i];
         break
       }
-  })
-})
+  });
+
+  $scope.answersRegister = [0,0,0,0];
+  $scope.quizOngoing = false;
+
+  $scope.startQuiz = function () {
+    $scope.quizOngoing = true;
+  };
+
+  $scope.selectOption = function (parentIndex,index) {
+    $scope.answersRegister[parentIndex] = index;
+    console.log($scope.answersRegister);
+  };
+
+});
 
 app.controller('addQuizCtrl', ['$scope', function ($scope) {
 
-}])
+}]);
