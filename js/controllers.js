de2cd988d5b28ed704f48cdcffcd7a182357dfd5
Abhinav Kumar <abhinav@outlook.com>
2016-12-09 19:14:40 +0530

    Fake loginSvc

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,36 @@
-app.controller('homeCtrl', ['$scope', function ($scope) {
+app.controller('homeCtrl', function ($scope, $location, loginSvc) {
 
-}]);
+  $scope.authorLogin = function (name) {
+    loginSvc.authorSignin(name);
+  };
 
-app.controller('mainCtrl', ['$scope', function ($scope) {
+  $scope.candidateLogin = function (name) {
+    loginSvc.candidateSignin(name)
+  }
+});
 
-}]);
+app.controller('mainCtrl', function ($scope, $location, loginSvc) {
+  $scope.username = loginSvc.getUsername();
+  $scope.currentRole = loginSvc.getCurrentRole();
+
+  $scope.$watch(function () {
+    return loginSvc.getUsername();
+
+  }, function (nval, oval) {
+    $scope.username = nval;
+  });
+
+  $scope.$watch(function () {
+    return loginSvc.getCurrentRole();
+
+  }, function (nval, oval) {
+    $scope.currentRole = nval;
+  });
+
+  $scope.switchRole = function () {
+    loginSvc.switchRole();
+  };
+});
 
 app.controller('quizListCtrl', function ($scope, quizService) {
 
@@ -28,14 +54,14 @@ app.controller('quizCtrl', function ($scope, $routeParams, quizService) {
       }
   });
 
-  $scope.answersRegister = [0,0,0,0];
+  $scope.answersRegister = [-1, -1, -1, -1];
   $scope.quizOngoing = false;
 
   $scope.startQuiz = function () {
     $scope.quizOngoing = true;
   };
 
-  $scope.selectOption = function (parentIndex,index) {
+  $scope.selectOption = function (parentIndex, index) {
     $scope.answersRegister[parentIndex] = index;
     console.log($scope.answersRegister);
   };
--- js/services.js
+++ js/services.js
@@ -1,18 +1,45 @@
 app.factory('quizService', ['$http', function ($http) {
 
-    var url = './data/quizzes.json'
-    var quizSvc = {}
+  var url = './data/quizzes.json';
+  var quizSvc = {};
 
-    getData = function () {
-        return $http.get(url).then(function (res) {
-            return res.data
-        })
+  getData = function () {
+    return $http.get(url).then(function (res) {
+      return res.data
+    })
+  };
+
+  var quizData = getData();
+  quizSvc.getAllQuizzes = function () {
+    return quizData
+  };
+
+  return quizSvc
+}]);
+
+
+app.factory('loginSvc', function () {
+  var username = null;
+  var currentRole = null;
+
+  return {
+    authorSignin: function (_username) {
+      console.log(_username);
+      currentRole = 'author';
+      username = _username;
+    },
+    candidateSignin: function (_username) {
+      currentRole = 'candidate';
+      username = _username;
+    },
+    getCurrentRole: function () {
+      return currentRole;
+    },
+    getUsername: function () {
+      return username;
+    },
+    switchRole: function () {
+      if(currentRole === 'author') currentRole = 'candidate'; else currentRole = 'author';
     }
-
-    var quizData = getData()
-    quizSvc.getAllQuizzes = function () {
-        return quizData
-    }
-
-    return quizSvc
-}])
+  }
+});
